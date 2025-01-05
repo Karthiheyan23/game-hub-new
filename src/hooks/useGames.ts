bf0b2ef1b5f5a96b0d382f62109d3ead19748183
Karthiheyan23 <karthiheyanofficial23@gmail.com>
2025-01-05 08:09:57 +0530

    FEAT : Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,7 @@ export interface Game {
   background_image: String;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
